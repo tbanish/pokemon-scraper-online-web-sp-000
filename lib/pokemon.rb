70a3f33e1f41c16b47657170f61c45bc8caa5087
Tom Banish <tombanish@gmail.com>
2020-06-10 02:47:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,8 @@ class Pokemon
       SQL
       
      DB[:conn].execute(sql, self.name, self.type, self.db)
-     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+   end
   end
   
   
